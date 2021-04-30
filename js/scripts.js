92eb697d61368fb5ac767754de74c5777122d193
“Arindam <18bcs4528@cuchd.in>
2021-04-30 13:41:25 +0530

    make it better

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('#reserveButton').click(function(){
+		$('#reserveTable').modal('show')
+	});
+	$('#login').click(function(){
+		$('#loginModal').modal('show')
+	});
+});
